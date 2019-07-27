b977875ce37cf91cb69b3a1e7516225d84505dab
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-07-28 01:42:58 +0300

    structures, got layers and function got finding a dict with special key among lists

--- kicad_parse.py
+++ kicad_parse.py
@@ -1,5 +1,8 @@
 from pyparsing import OneOrMore, nestedExpr, Dict
+from typing import Dict, Any, List
+import pprint
 
+from pcb_structure import *
 
 def list_to_dict(data: list) -> dict:
     """
@@ -12,6 +15,11 @@ def list_to_dict(data: list) -> dict:
         return res
     if len(data) == 2:
         res[data[0]] = data[1]
+        if not isinstance(res[data[0]], list):
+            try:
+                res[data[0]] = float(res[data[0]])
+            except ValueError:
+                pass
     else:
         res[data[0]] = data[1:]
     current_data = res[data[0]]
@@ -23,11 +31,62 @@ def list_to_dict(data: list) -> dict:
 
     return res
 
+
+def get_dict_by_key(data: List[Dict[str, Any]], key: str) -> Dict[str, Any]:
+    """
+    find a dict in a list with selected key
+    :param key: key to find
+    :param data: list with dicts
+    :return: dict with given key
+    """
+    for d in data:
+        if key in d.keys():
+            return d
+    return {}
+
+# def upgrade_lists_in_dict(data: Dict[str, any]):
+#     """
+#
+#    :param data:
+#    :return:
+#    """
+#    for key in data.keys():
+#        if isinstance(data[key], list):
+#            new = {}
+
+
+def get_layers(data: Dict[str, Any]) -> List[Layer]:
+    """
+    get layers from kicad pcb file
+    :param data:
+    :return:
+    """
+    try:
+        layers = data['kicad_pcb']
+        layers = get_dict_by_key(layers, 'layers')
+        res: List[Layer] = list()
+        for layer in layers['layers']:
+            layer_data = list(layer.values())[0]
+            new_layer = Layer(name=layer_data[0], layer_type=layer_data[1])
+            res.append(new_layer)
+        print(res)
+        return res
+
+    except KeyError:
+        print("Wrong file structure, unable to get layers")
+
+
 if __name__ == '__main__':
-    inputdata = open("FireFly.kicad_pcb").read()
-    data = OneOrMore(nestedExpr()).parseString(inputdata)
-    data = data.asList()
-    a = list_to_dict(data[0])
-    print(a)
+    inputdata = open("data/FireFly.kicad_pcb").read()
+    data_parse = OneOrMore(nestedExpr()).parseString(inputdata)
+    data_list = data_parse.asList()
+    data = list_to_dict(data_list[0])
+    text: str = pprint.pformat(data, indent=0)
+    with open("dump.txt", "w") as f:
+        f.write(text)
+    print(data)
+    layers = get_layers(data)
+    pcb = PCB(layers, list())
+
     pass
 
